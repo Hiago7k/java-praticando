9d4a17883d8df1895f721c1fb218540456478d39
Hiago 7k <hiago7k@gmail.com>
2026-06-29 19:28:52 -0300

    logica basica do sistema

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,28 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args)
     {
-        System.out.println("Iniciando projeto java");
-        System.out.println("Iniciando projeto java");
-        System.out.println("Iniciando projeto java");
-        System.out.println("Iniciando projeto java");
+        // Copa Do mundo 2026
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("|********************************|");
+        System.out.println("|------ Copa do mundo 2026 ------|");
+        System.out.println("|********************************|");
+
+        System.out.println("Digite o primeiro time que passou para as oitivas de final: ");
+        String time1 = teclado.next();
+        System.out.println("Digite o segundo time que passou para as oitivas de final: ");
+        String time2 = teclado.next();
+        System.out.println("Digite o terceiro time que passou para as oitivas de final: ");
+        String time3 = teclado.next();
+        System.out.println("Digite o quarto time que passou para as oitivas de final: ");
+        String time4 = teclado.next();
+
+        System.out.println("Os times classificados foram: ");
+        System.out.println("- " + time1 + " -" + "- " + time2 + " -" + "- " + time3 + " -" + "- " + time4 + " -");
+        
     }
 }
